2bc745a73f606d9b4ecd5e9eda30b8063bf04226
jhkimon <sky06098i1@gmail.com>
2025-02-12 12:40:36 +0900

    misc. 커리큘럼 navbar

--- styles/activities/components/curriculum/style.ts
+++ styles/activities/components/curriculum/style.ts
@@ -1,236 +1,230 @@
-import styled, { css, keyframes } from "styled-components";
-import { THEME } from "styles/theme";
+import styled, { css, keyframes } from 'styled-components';
+import { THEME } from 'styles/theme';
 
 export const Container = styled.div<{ isMobile: boolean }>`
-  width: 100%;
-  padding: 6rem 3rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
+    width: 100%;
+    padding: 6rem 3rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 
-  ${(props) =>
-    props.isMobile &&
-    css`
-      padding: 2rem 2rem;
-    `}
+    ${(props) =>
+        props.isMobile &&
+        css`
+            padding: 2rem 2rem;
+        `}
 `;
 
 export const MainContainer = styled.div<{ isMobile: boolean }>`
-  width: 80%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-
-  ${(props) =>
-    props.isMobile &&
-    css`
-      width: 100%;
-    `}
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
 `;
 
 export const TextWrapper = styled.div<{ isMobile: boolean }>`
-  flex: 1;
-  min-width: 300px;
-  margin-left: 20%;
-  width: 100%;
-  word-break: keep-all;
-  color: #333333;
-  overflow: hidden;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-
-  & p {
-    line-height: 150%;
-  }
-
-  & .main-text {
-    font-size: 2.4rem;
-    color: ${THEME.ORANGE};
-    margin-bottom: 3rem;
-  }
-
-  & .sub-text {
-    font-size: 1.8rem;
-    font-weight: 400;
+    flex: 1;
+    min-width: 300px;
+    margin-left: 20%;
+    width: 100%;
+    word-break: keep-all;
     color: #333333;
-  }
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    flex-direction: column;
 
-  & .test {
-    font-size: 1.5rem;
-    margin-top: 4rem;
-  }
+    & p {
+        line-height: 150%;
+    }
 
-  ${(props) =>
-    props.isMobile &&
-    css`
-      & .main-text {
-        font-size: 2rem;
+    & .main-text {
+        font-size: 2.4rem;
         color: ${THEME.ORANGE};
-      }
+        margin-bottom: 3rem;
+    }
 
-      & .sub-text {
-        font-size: 1.5rem;
+    & .sub-text {
+        font-size: 1.8rem;
         font-weight: 400;
         color: #333333;
-      }
+    }
 
-      & .test {
+    & .test {
         font-size: 1.5rem;
         margin-top: 4rem;
-      }
+    }
 
-      & img {
-        width: 100%;
-      }
-      & p {
-        width: 100%;
-      }
-    `}
+    ${(props) =>
+        props.isMobile &&
+        css`
+            & .main-text {
+                font-size: 2rem;
+                color: ${THEME.ORANGE};
+            }
+
+            & .sub-text {
+                font-size: 1.5rem;
+                font-weight: 400;
+                color: #333333;
+            }
+
+            & .test {
+                font-size: 1.5rem;
+                margin-top: 4rem;
+            }
+
+            & img {
+                width: 100%;
+            }
+            & p {
+                width: 100%;
+            }
+        `}
 `;
 
 export const LottieContainer = styled.div<{ isMobile: boolean }>`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  padding: 6rem 10%;
-  position: relative;
-  /* background: radial-gradient(
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 6rem 10%;
+    position: relative;
+    /* background: radial-gradient(
     circle farthest-side at 50% 100%,
     rgba(255, 255, 255, 0.5),
     rgba(255, 255, 255, 0)
   ); */
-  /* background: linear-gradient(); */
-  ${(props) =>
-    props.isMobile &&
-    css`
-      flex-direction: column;
-      align-items: center;
-      padding: 0rem 7rem;
-    `}
+    /* background: linear-gradient(); */
+    ${(props) =>
+        props.isMobile &&
+        css`
+            flex-direction: column;
+            align-items: center;
+            padding: 0rem 7rem;
+        `}
 
-  @media screen and (max-width: 530px) {
-    padding: 0;
-  }
+    @media screen and (max-width: 530px) {
+        padding: 0;
+    }
 `;
 
 export const ScheduleContainer = styled.div<{ isMobile: boolean }>`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  padding: 6rem 0;
-  position: relative;
-  flex-wrap: wrap;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 6rem 0;
+    position: relative;
+    flex-wrap: wrap;
 
-  @media screen and (max-width: 1281px) {
-    width: 90%;
-    margin: 0 auto;
-  }
+    @media screen and (max-width: 1281px) {
+        width: 90%;
+        margin: 0 auto;
+    }
 
-  @media screen and (max-width: 1030px) {
-    justify-content: center;
-  }
+    @media screen and (max-width: 1030px) {
+        justify-content: center;
+    }
 
-  ${(props) =>
-    props.isMobile &&
-    css`
-      flex-direction: column;
-      align-items: center;
-    `}
+    ${(props) =>
+        props.isMobile &&
+        css`
+            flex-direction: column;
+            align-items: center;
+        `}
 
-  & > div:first-child > div > p {
-    right: -2rem;
-  }
+    & > div:first-child > div > p {
+        right: -2rem;
+    }
 
-  @media screen and (max-width: 954px) {
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 3rem;
-  }
+    @media screen and (max-width: 954px) {
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 3rem;
+    }
 `;
 
 export const ScheduleWrapper = styled.div<{ isMobile: boolean }>`
-  width: 25%;
-  height: 18rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  & h2 {
-    font-size: 3rem;
-    font-weight: 500;
-  }
-  & p {
-    font-size: 1.8rem;
-    line-height: 150%;
-    margin: 1rem;
-  }
-
-  @media screen and (max-width: 1680px) {
-    width: 21%;
-    height: 13rem;
+    width: 25%;
+    height: 18rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     & h2 {
-      font-size: 2rem;
-      font-weight: 500;
+        font-size: 3rem;
+        font-weight: 500;
     }
     & p {
-      font-size: 1.3rem;
-      line-height: 150%;
-      margin: 1rem;
+        font-size: 1.8rem;
+        line-height: 150%;
+        margin: 1rem;
     }
-  }
 
-  @media screen and (max-width: 1281px) {
-    width: 34rem;
-    height: 13rem;
-    margin-bottom: 6rem;
-  }
+    @media screen and (max-width: 1680px) {
+        width: 21%;
+        height: 13rem;
 
-  ${(props) =>
-    props.isMobile &&
-    css`
-      width: 80%;
-      margin-bottom: 6rem;
-    `}
+        & h2 {
+            font-size: 2rem;
+            font-weight: 500;
+        }
+        & p {
+            font-size: 1.3rem;
+            line-height: 150%;
+            margin: 1rem;
+        }
+    }
 
-  @media screen and (max-width: 300px) {
-    width: 100%;
-    height: 12rem;
-  }
+    @media screen and (max-width: 1281px) {
+        width: 34rem;
+        height: 13rem;
+        margin-bottom: 6rem;
+    }
+
+    ${(props) =>
+        props.isMobile &&
+        css`
+            width: 80%;
+            margin-bottom: 6rem;
+        `}
+
+    @media screen and (max-width: 300px) {
+        width: 100%;
+        height: 12rem;
+    }
 `;
 
 export const ScheduleEventBox = styled.div`
-  position: relative;
-  width: 13rem;
-  height: 90%;
-  background-color: ${THEME.ORANGE};
-  font-weight: 700;
-  margin-right: 1.3rem;
-
-  & p {
-    position: absolute;
-    bottom: -2rem;
-    right: -1rem;
-    font-size: 9rem;
-    line-height: 110%;
-    color: #ffffff;
-  }
-
-  @media screen and (max-width: 1680px) {
-    width: 9rem;
-    height: 12rem;
-    margin-bottom: 0rem;
+    position: relative;
+    width: 13rem;
+    height: 90%;
+    background-color: ${THEME.ORANGE};
+    font-weight: 700;
+    margin-right: 1.3rem;
 
     & p {
-      font-size: 6rem;
+        position: absolute;
+        bottom: -2rem;
+        right: -1rem;
+        font-size: 9rem;
+        line-height: 110%;
+        color: #ffffff;
     }
 
-    /* @media screen and (max-width: 1300px) {
+    @media screen and (max-width: 1680px) {
+        width: 9rem;
+        height: 12rem;
+        margin-bottom: 0rem;
+
+        & p {
+            font-size: 6rem;
+        }
+
+        /* @media screen and (max-width: 1300px) {
     width: 80%;
     height: 15rem;
     margin-bottom: 6rem;
@@ -238,19 +232,18 @@ export const ScheduleEventBox = styled.div`
     & p {
       font-size: 9rem;
     } */
-  }
+    }
 `;
 
 export const ScheduleInfoBox = styled.div`
-  width: 60%;
-  height: 100%;
-  display: flex;
-  align-items: start;
-  justify-content: space-between;
-  flex-direction: column;
+    width: 60%;
+    height: 100%;
+    display: flex;
+    align-items: start;
+    justify-content: space-between;
+    flex-direction: column;
 
-  & p {
-    color: #5f5f5f;
-  }
+    & p {
+        color: #5f5f5f;
+    }
 `;
-
--- styles/activities/components/demoday/index.ts
+++ styles/activities/components/demoday/index.ts
@@ -1,64 +1,68 @@
-import styled, { css, keyframes } from "styled-components";
-import { THEME } from "styles/theme";
+import styled, { css, keyframes } from 'styled-components';
+import { THEME } from 'styles/theme';
 
 export const Container = styled.div<{ isMobile: boolean }>`
-  width: 100%;
-  min-height: 100%;
-  padding: 6rem 3rem;
+    width: 100%;
+    min-height: 100%;
+    padding: 6rem 3rem;
+
+    ${(props) =>
+        props.isMobile &&
+        css`
+            padding: 2rem 2rem;
+        `}
 `;
 
 export const MainContainer = styled.div<{ isMobile: boolean }>`
-  width: 70%;
-  height: 100%;
-  margin: 0 auto;
-  padding-top: 5rem;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  box-sizing: border-box;
-
-
-  & > div:last-child {
-    border-right: none;
-  }
-
-  @media screen and (max-width: 1830px) {
     width: 70%;
-  }
-
-  @media screen and (max-width: 1530px) {
-    width: 80%;
-  }
-
-  @media screen and (max-width: 1350px) {
-    width: 90%;
-  }
-
-  @media screen and (max-width: 1220px) {
-    width: 100%;
-  }
-
-  @media screen and (max-width: 1104px) {
-    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    padding-top: 5rem;
+    display: flex;
+    flex-wrap: wrap;
     justify-content: center;
+    box-sizing: border-box;
 
-    & > div:first-child {
-      margin-bottom: 15rem;
+    & > div:last-child {
+        border-right: none;
     }
-  }
+
+    @media screen and (max-width: 1830px) {
+        width: 70%;
+    }
+
+    @media screen and (max-width: 1530px) {
+        width: 80%;
+    }
+
+    @media screen and (max-width: 1350px) {
+        width: 90%;
+    }
+
+    @media screen and (max-width: 1220px) {
+        width: 100%;
+    }
+
+    @media screen and (max-width: 1104px) {
+        width: 80%;
+        justify-content: center;
+
+        & > div:first-child {
+            margin-bottom: 15rem;
+        }
+    }
 `;
 
 export const SessionWrapper = styled.div<{ isMobile: boolean }>`
-  position: relative;
-  width: 50%;
-  height: 32rem;
-  text-align: center;
+    position: relative;
+    width: 70%;
+    height: 32rem;
+    text-align: center;
     &:first-child {
-    margin-top: 2%;
-  }
+        margin-top: 2%;
+    }
 
-
-  /* &:after {
+    /* &:after {
     content: "";
     position: absolute;
     top: 0;
@@ -68,39 +72,39 @@ export const SessionWrapper = styled.div<{ isMobile: boolean }>`
     display: block;
   } */
 
-  @media screen and (max-width: 710px) {
-    width: 30rem;
-    height: 30rem;
-    &:after {
-      display: none;
+    @media screen and (max-width: 710px) {
+        width: 30rem;
+        height: 30rem;
+        &:after {
+            display: none;
+        }
     }
-  }
 `;
 
 export const SessionImgBox = styled.div<{ isMobile: boolean }>`
-  width: 50rem;
-  height: 20rem;
-  margin: 0 auto;
-  margin-top: 2rem;
-  margin-bottom: 3rem;
-  padding-right: 1rem;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-
-  @media screen and (max-width: 745px) {
-    width: 20rem;
-    height: 14rem;
-  }
-
-  @media screen and (max-width: 710px) {
-    width: 20rem;
-    height: 10rem;
+    width: 50rem;
+    height: 20rem;
     margin: 0 auto;
-  }
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+    padding-right: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 
-  /* &:before {
+    @media screen and (max-width: 745px) {
+        width: 20rem;
+        height: 14rem;
+    }
+
+    @media screen and (max-width: 710px) {
+        width: 20rem;
+        height: 10rem;
+        margin: 0 auto;
+    }
+
+    /* &:before {
     content: "";
     width: 100%;
     padding-bottom: 50%;
@@ -108,105 +112,105 @@ export const SessionImgBox = styled.div<{ isMobile: boolean }>`
 `;
 
 export const SessionTitleBox = styled.p<{ isMobile: boolean }>`
-  width: 90%;
-  margin: 0 auto;
-  margin-bottom: 1.5rem;
-  color: ${THEME.ORANGE};
-  font-size: 2.4rem;
-  font-weight: 700;
-  text-align: center;
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 1.5rem;
+    color: ${THEME.ORANGE};
+    font-size: 2.4rem;
+    font-weight: 700;
+    text-align: center;
 
-  @media screen and (max-width: 710px) {
-    font-size: 1.8rem;
-    font-weight: 600;
-  }
+    @media screen and (max-width: 710px) {
+        font-size: 1.8rem;
+        font-weight: 600;
+    }
 `;
 
 export const SessionInfoBox = styled.p<{ isMobile: boolean }>`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 9rem;
-  & p {
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 9rem;
+    & p {
+        width: 100%;
 
-    margin: 0 auto;
-    word-break: keep-all;
-    white-space: pre-wrap;
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-    font-weight: 600;
-  }
-  margin-bottom: 5rem;
+        margin: 0 auto;
+        word-break: keep-all;
+        white-space: pre-wrap;
+        font-size: 1.5rem;
+        line-height: 2.5rem;
+        font-weight: 600;
+    }
+    margin-bottom: 5rem;
 
-  @media screen and (max-width: 710px) {
-    font-size: 1.5rem;
-    font-weight: 600;
-    margin-bottom: 1rem;
-    height: auto;
-  }
+    @media screen and (max-width: 710px) {
+        font-size: 1.5rem;
+        font-weight: 600;
+        margin-bottom: 1rem;
+        height: auto;
+    }
 `;
 
 export const SessionDayBox = styled.p<{ isMobile: boolean }>`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 9rem;
-  & p {
-    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 9rem;
+    & p {
+        width: 90%;
 
-    margin: 0 auto;
-    word-break: keep-all;
-    white-space: pre-wrap;
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-    font-weight: 600;
-  }
-  margin-bottom: 5rem;
+        margin: 0 auto;
+        word-break: keep-all;
+        white-space: pre-wrap;
+        font-size: 1.5rem;
+        line-height: 2.5rem;
+        font-weight: 600;
+    }
+    margin-bottom: 5rem;
 
-  @media screen and (max-width: 710px) {
-    font-size: 1.5rem;
-    font-weight: 600;
-    margin-bottom: 4rem;
-    margin-top: 5rem;
-  }
+    @media screen and (max-width: 710px) {
+        font-size: 1.5rem;
+        font-weight: 600;
+        margin-bottom: 4rem;
+        margin-top: 5rem;
+    }
 `;
 
 export const ProjectExampleWrapper = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  margin-bottom: 3rem;
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 3rem;
 `;
 
 export const ProjectTextBox = styled.p`
-  font-weight: 600;
-  max-width: 30.3rem;
-  height: 4rem;
-  margin: 0 auto;
-  background-color: ${THEME.BLACK_200};
-  color: ${THEME.WHITE};
-  font-size: 2rem;
-  line-height: 4rem;
-  text-align: center;
+    font-weight: 600;
+    max-width: 30.3rem;
+    height: 4rem;
+    margin: 0 auto;
+    background-color: ${THEME.BLACK_200};
+    color: ${THEME.WHITE};
+    font-size: 2rem;
+    line-height: 4rem;
+    text-align: center;
 `;
 
 export const ProjectImgBox = styled.p`
-  width: 100%;
+    width: 100%;
 `;
 
 export const SlideWrapper = styled.p`
-  width: 100%;
-  margin: 0 auto;
+    width: 100%;
+    margin: 0 auto;
 `;
 
 export const ProjectUpdateText = styled.p`
-  width: 100%;
-  margin: 0 auto;
-  font-size: 2.5rem;
-  text-align: center;
-  font-weight: 700;
+    width: 100%;
+    margin: 0 auto;
+    font-size: 2.5rem;
+    text-align: center;
+    font-weight: 700;
 
-  & > span {
-    color: ${THEME.ORANGE};
-  }
+    & > span {
+        color: ${THEME.ORANGE};
+    }
 `;
